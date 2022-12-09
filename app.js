0a72d6ffbf4037dc861f2f5186914829de973606
Dmitry Pirogov <pirogovds98@gmail.com>
2022-12-09 11:26:02 +0300

    Added hash work

--- app.js
+++ app.js
@@ -23,30 +23,34 @@ document.addEventListener('click', event =>{
     }
 })
 
-function generateRandomColor(){
-
-    const hexCodes = '0123456789ABCDEF'
-    let color = ''
-    for (let i = 0; i < 6; i++) {
-        color += hexCodes[Math.floor(Math.random() *hexCodes.length)]
-    }
-    return '#' + color
-}
-
 function copyToClickboard(text){
     return  navigator.clipboard.writeText(text)
 }
 
-function setRandomColors(){
- cols.forEach(col => {
+function setRandomColors(isInitial){
+ const colors = isInitial ? getColorsFromHash() : []
+
+ cols.forEach((col, index )=> {
      const isLocked = col.querySelector('i').classList.contains('fa-lock')
      const text = col.querySelector('h2')
      const button = col.querySelector('button')
-     const color = chroma.random()
+
 
      if (isLocked){
+         colors.push(text.textContent)
          return
      }
+
+     const color = isInitial
+         ? colors[index]
+         ? colors[index]
+             :chroma.random()
+         : chroma.random()
+
+     if(!isInitial){
+         colors.push(color)
+     }
+     colors.push(color)
 
      text.textContent = color
      col.style.background = color
@@ -54,6 +58,8 @@ function setRandomColors(){
      setTextColor(text, color)
      setTextColor(button, color)
  })
+
+    updateColorsHash(colors)
 }
 
 function setTextColor(text, color){
@@ -61,5 +67,17 @@ function setTextColor(text, color){
     text.style.color = luminance > 0.5 ? 'black' : 'white'
 }
 
+function updateColorsHash(colors = []){
+    document.location.hash = colors.map((col) =>{
+      return   col.toString().substring(1)
+    }).join('-')
+}
 
-setRandomColors()
+function getColorsFromHash(){
+    if(document.location.hash.length >1){
+       return document.location.hash.substring(1).split('-').map(color => '#' + color)
+    }
+    return[]
+}
+
+setRandomColors(true)
